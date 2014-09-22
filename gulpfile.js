efddfea6d9e5207f2092954245c6639bf51e65cf
Sinjie.Wang(王信傑) <sinjie.wang@vivotek.com>
2014-09-22 16:59:49 +0800

    [A] add fonts forder

--- gulpfile.js
+++ gulpfile.js
@@ -50,10 +50,10 @@ gulp.task('images', function () {
 });
 
 gulp.task('fonts', function () {
-    return $.bowerFiles()
+    return gulp.src(require('main-bower-files')().concat('app/fonts/**/*'))
         .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
         .pipe($.flatten())
-        .pipe(gulp.dest('dist/fonts'))
+        .pipe(gulp.dest('dist/styles'))
         .pipe($.size());
 });
 
