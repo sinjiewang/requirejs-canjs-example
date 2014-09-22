05ff943a091ea4dd3a58b86e79caef2db1496c5c
Sinjie.Wang(王信傑) <sinjie.wang@vivotek.com>
2014-09-22 17:07:44 +0800

    [F] fix fonts path

--- gulpfile.js
+++ gulpfile.js
@@ -53,7 +53,7 @@ gulp.task('fonts', function () {
     return gulp.src(require('main-bower-files')().concat('app/fonts/**/*'))
         .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
         .pipe($.flatten())
-        .pipe(gulp.dest('dist/styles'))
+        .pipe(gulp.dest('dist/fonts'))
         .pipe($.size());
 });
 
